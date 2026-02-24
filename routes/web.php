5d2c3640e453e476b007d95747e20a85dc7d66a5
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2026-02-24 14:08:50 +0300

    Changed names in some routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::middleware(['throttle:10,1'])->group(function () {
         Route::post('/v1/tasks', 'store')->name('store');
         Route::get('/v1/tasks', 'index')->name('index');
         Route::get('/v1/tasks/{id}', 'show')->name('show');
-        Route::put('/v1/tasks/{id}', 'update')->name('update');
-        Route::delete('/v1/tasks/{id}', 'delete')->name('delete');
+        Route::patch('/v1/tasks/{id}', 'update')->name('update');
+        Route::delete('/v1/tasks/{id}', 'destroy')->name('destroy');
     });
 });
