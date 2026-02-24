909414387794b03836bc731e964d105039a812f7
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2026-02-24 14:09:57 +0300

    All necessary methods added

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -5,70 +5,66 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Http\JsonResponse;
 
 class TasksController extends Controller
 {
     public function store(Request $request): JsonResponse
     {
-        try {
-            $request->validate([
-                'title' => 'required|string|max:255',
-                'description' => 'string|max:1024'
-            ]);
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string|max:1024',
+            'status' => 'nullable|boolean'
+        ]);
 
-            $task = Task::create($request->only(['title', 'description', 'status']));
+        $task = Task::create($data);
 
+        Log::info("Task created successfully", ['id' => $task->id]);
 
-
-            Log::info("Task created successfully");
-
-            return response()->json([
-                'success' => true,
-                'title' => $request->input('title'),
-                'description' => $request->input('description'),
-                'status' => $request->input('status')
-            ], 200);
-
-        } catch (ValidationException $e) {
-            Log::error("Failed task creation!");
-
-            return response()->json([
-                'success' => false,
-                'errors' => $e->errors()
-            ], 422);
-        }
+        return response()->json($task, 201);
     }
 
-    public function index()
+    public function index(): JsonResponse
     {
-        return Task::orderBy('created_at')->get();
+        $tasks = Task::orderBy('created_at')->get();
+
+        Log::info("List of tasks was returned");
+
+        return response()->json($tasks);
     }
 
-    public function show($id)
-    {
-        return Task::findOrFail($id);
-    }
-
-    public function update(Request $request, $id)
+    public function show($id): JsonResponse
     {
         $task = Task::findOrFail($id);
-        $task->update($request->only(['title', 'description', 'status']));
+        Log::info('Task found', ['id' => $task->id]);
+        return response()->json($task);
     }
 
-    public function delete($id): JsonResponse
+    public function update(Request $request, $id): JsonResponse
     {
-        // $task = Task::findOrFail($id);
-        // $task->delete();
-
-        $destroyTask = Task::destroy($id);
-
-        return response()->json([
-            'success' => $destroyTask,
-            'task id' => $id
+        $data = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string|max:1024',
+            'status' => 'sometimes|boolean',
         ]);
+
+        $task = Task::findOrFail($id);
+
+        $task->update($data);
+
+        Log::info('Task updated successfully', ['id' => $task->id]);
+
+        return response()->json($task, 200);
     }
 
+    public function destroy($id): JsonResponse
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        Log::info('Task deleted', ['id' => $task->id]);
+
+        return response()->json(null, 204);
+    }
 
 }
